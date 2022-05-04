50287c260ce80d922944eaa755771c2ab4a7d55d
alebuiko <aleksandr.buiko@t-systems.com>
2022-05-04 15:28:34 +0300

    use ListItemLayout component

---
+++ src/components/ListItemLayout.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps({
+  head: Boolean,
+});
+</script>
+
+<template>
+  <div
+    class="list-item-layout"
+    :class="{ head }"
+  >
+    <div class="cell">
+      <slot name="foo" />
+    </div>
+
+    <div class="cell">
+      <slot name="bar" />
+    </div>
+
+    <div class="cell">
+      <slot name="baz" />
+    </div>
+
+    <div class="cell">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.list-item-layout {
+  display: flex;
+}
+
+.head {
+  font-weight: bold;
+  color: gray;
+  margin-bottom: 24px;
+}
+
+.cell {
+  flex: 1;
+  min-width: 0;
+}
+</style>
--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ListItemLayout from './ListItemLayout.vue';
 import { useTheList } from '../setup/use-the-list';
 
 const { listData, removeListItem } = useTheList();
@@ -6,26 +7,57 @@ const { listData, removeListItem } = useTheList();
 
 <template>
   <ul class="the-list">
+    <ListItemLayout head>
+      <template #foo>
+        foo
+      </template>
+
+      <template #bar>
+        bar
+      </template>
+
+      <template #baz>
+        baz
+      </template>
+
+      <template #actions>
+        actions
+      </template>
+    </ListItemLayout>
+
     <li
       v-for="(item, index) of listData"
       :key="index"
       class="list-item"
     >
-      <div>{{ item.foo }}</div>
-      <div>{{ item.bar }}</div>
-      <div>{{ item.baz }}</div>
-      <div>
-        <scale-button
-          icon-only
-          variant="secondary"
-          @click="removeListItem(index)"
-        >
-          <scale-icon
-            name="close-circle"
-            accessibility-title="remove"
-          />
-        </scale-button>
-      </div>
+      <ListItemLayout>
+        <template #foo>
+          <div>{{ item.foo }}</div>
+        </template>
+
+        <template #bar>
+          <div>{{ item.bar }}</div>
+        </template>
+
+        <template #baz>
+          <div>{{ item.baz }}</div>
+        </template>
+
+        <template #actions>
+          <div>
+            <scale-button
+              icon-only
+              variant="secondary"
+              @click="removeListItem(index)"
+            >
+              <scale-icon
+                name="close-circle"
+                accessibility-title="remove"
+              />
+            </scale-button>
+          </div>
+        </template>
+      </ListItemLayout>
     </li>
   </ul>
 </template>
@@ -34,12 +66,10 @@ const { listData, removeListItem } = useTheList();
 .the-list {
   padding-left: 0;
   margin: 24px 0;
+  list-style: none;
 }
 
 .list-item {
-  display: flex;
-  justify-content: space-between;
-
   &:not(:first-child) {
     margin-top: 8px;
   }
